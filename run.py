0a4861d617f0719bb8442ab6a5490ed91ba292a2
Sadi Uysal <sadiuysalsadi@gmail.com>
2020-12-13 16:12:10 +0300

    error_handling

--- run.py
+++ run.py
@@ -55,7 +55,7 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(32, 32)),
-  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(96, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10)
 ])
