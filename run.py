e9dd1a491f3dcd64e47d75eb2044107025105811
Sadi Uysal <sadiuysalsadi@gmail.com>
2020-12-13 16:32:27 +0300

    error_handling

--- run.py
+++ run.py
@@ -56,12 +56,14 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
   tf.keras.layers.MaxPooling2D((2, 2)),
+  tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(64, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10)
 ])
 
+model.summary
 
 dataset_size=int(tf.shape(x_train)[0])
 y_train_sets=y_train
