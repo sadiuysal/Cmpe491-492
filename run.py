1ea2e47d56616c02859d816ca120b76547ababd8
Sadi Uysal <sadiuysalsadi@gmail.com>
2020-12-13 19:15:33 +0300

    error_handling

--- run.py
+++ run.py
@@ -87,7 +87,7 @@ for ind in range(dataset_size):
   x_train_sets[ind] = t_x
   t_prime_x=tf.expand_dims(data_util.preprocess_for_train(x,height,width),0)
   y_train_sets[ind] = model(t_prime_x)
-tf.expand_dims(x_train_sets,0)
+x_train_sets=tf.expand_dims(x_train_sets,0)
 
 """For each example the model returns a vector of "[logits](https://developers.google.com/machine-learning/glossary#logits)" or "[log-odds](https://developers.google.com/machine-learning/glossary#log-odds)" scores, one for each class."""
 print(tf.shape(x_train_sets[1]))
