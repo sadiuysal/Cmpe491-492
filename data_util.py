954b08626d53fc0ae242246028fe09d02df9f452
Sadi Uysal <sadiuysalsadi@gmail.com>
2020-12-20 15:54:16 +0300

    error handling

--- data_util.py
+++ data_util.py
@@ -9,10 +9,11 @@ from scipy import spatial
 
 #cosine similarity_with_temperature
 def sim_with_temperature(x,y,temperature):
-  print(x)
-  print(y)
-  cos_sim=1 - spatial.distance.cosine(x, y)
-  """m = tf.keras.metrics.CosineSimilarity(axis=1)
-  m.update_state(x, y)"""
-  return cos_sim/temperature
+  #print(x)
+  #print(y)
+  #cos_sim=1 - spatial.distance.cosine(x, y)
+  m = tf.keras.metrics.CosineSimilarity(axis=1)
+  m.update_state(x, y)
+  return m.result().numpy()
+  #return cos_sim/temperature
 
