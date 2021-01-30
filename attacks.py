dce10235fcfab3ec405614818abe9ccae90a190e
Sadi Uysal <sadiuysalsadi@gmail.com>
2021-01-30 20:28:13 +0300

    Attacks library updated, recursion disabled, PGD corrections are done, scaling changed.

--- attacks.py
+++ attacks.py
@@ -2,7 +2,8 @@
 import tensorflow as tf
 import objective as obj_lib
 import data_util
-
+import numpy as np
+#TODO add uniform noise (-e,+e)
 def get_adversaries( model, x ,target,epsilon,itr_count):
   #print(tf.shape(x))
   #print(tf.shape(target))
@@ -19,7 +20,7 @@ def get_adversaries( model, x ,target,epsilon,itr_count):
   # Get the sign of the gradients to create the perturbation
   signed_grad = tf.sign(gradient)
   adv = x + (epsilon * signed_grad)
-  adversaries = data_util.cast_to_float32(tf.clip_by_value(adv, 0, 1))
+  adversaries = data_util.cast_to_float32(tf.clip_by_value(adv, 0, 1)) #TODO change to 0-255
   if itr_count>1:
     return get_adversaries(model,adversaries,target,epsilon,itr_count-1)
   else:
@@ -29,4 +30,30 @@ def get_adversaries( model, x ,target,epsilon,itr_count):
     # print(tf.shape(adversaries))
     return adversaries,loss
 
+def get_adversaries_2( model, x ,target,epsilon,itr_count,step_size):  # not completed
+  perturbation = np.random.uniform(-epsilon,epsilon,tf.shape(x))
+  adv = x + perturbation
+  for itr in range(itr_count):
+    with tf.GradientTape() as tape:
+      tape.watch(adv)
+      inputs = tf.concat([adv, target], 0)
+      outputs=model(inputs, training=False)
+      loss = obj_lib.contrastive_Loss(outputs)
 
+    #print("Adversarial example finding itr count: "+str(itr)+" Loss: " + str(loss))
+    # Get the gradients of the loss w.r.t to the input image.
+    gradient = tape.gradient(loss, adv)
+    #print(gradient)
+    # Get the sign of the gradients to create the perturbation
+    signed_grad = tf.sign(gradient)
+    perturbation += (step_size * signed_grad)
+    perturbation = data_util.cast_to_float32(tf.clip_by_value(perturbation, -epsilon, epsilon))
+    adv = x + perturbation
+    adversaries = data_util.cast_to_float32(tf.clip_by_value(adv, 0, 255)) #TODO change to 0-255
+
+
+  inputs = tf.concat([adv, x], 0)
+  outputs = model(inputs, training=False)
+  loss = obj_lib.contrastive_Loss(outputs)
+  # print(tf.shape(adversaries))
+  return adversaries,loss
