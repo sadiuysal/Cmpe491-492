9e22250a07fa0c627f61cb16f0cb80743d0928a2
Şadi Uysal <sadiuysalsadi@gmail.com>
2021-04-19 17:07:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-batch_size = 128
+batch_size = 512
 IMG_SIZE = 32
 adversarial_attack = True
 epsilon = 8  # 0-255>>8
 _lambda = 256
-nof_epochs = 5
+nof_epochs = 500 # 
 attacks_itr_count = 7
 attacks_step_size = 1.8
