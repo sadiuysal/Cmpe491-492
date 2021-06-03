7a6652d26f02954db0842a2d31433552ecee45e0
I540653 <sadi.uysal@sap.com>
2021-06-03 05:14:24 +0300

    Hyper parameters optimized for personal computer

--- run.py
+++ run.py
@@ -26,150 +26,165 @@ import models
 import os
 from IPython import display
 
+tf.config.threading.set_inter_op_parallelism_threads(2)
+tf.config.threading.set_intra_op_parallelism_threads(2)
 
-"""Load and prepare the [MNIST dataset](http://yann.lecun.com/exdb/mnist/). Convert the samples from integers to floating-point numbers:"""
-
-#mnist = tf.keras.datasets.mnist
-cifar10 = tf.keras.datasets.cifar10
-
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-x_train, x_test = data_util.cast_to_float32 ( x_train ) , data_util.cast_to_float32( x_test )
-(x_train, y_train), (x_test, y_test) = (x_train[:1000,:], y_train[:1000,:]), (x_test[:100,:], y_test[:100,:])
-
-train_images = (x_train - 127.5) / 127.5  # Normalize the images to [-1, 1]
-
-# Batch and shuffle the data
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(cfg.BUFFER_SIZE).batch(cfg.BATCH_SIZE)
-
-#inputs = keras.Input(shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
-"""
-base_model = applications.resnet50.ResNet50(weights=None, include_top=False,
-                                            input_shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
-discriminator = tf.keras.applications.ResNet50(
-    include_top=False, weights='imagenet',
-    input_shape=(32,32,3), input_tensor=inputs,pooling=None
-)"""
+logical_devices = tf.config.list_logical_devices('CPU')
+print("Num CPUs:", len(logical_devices))
 
 
-#noise = tf.random.normal([1, 100])
-generator = models.make_generator_model(latent_dim=100)
-# summarize the model
-generator.summary()
+with tf.device(logical_devices[0].name):
 
-#generated_image = generator(noise, training=False)
-#print(generated_image[0, :, :, 0].shape)
-#print(x_train[0,:].shape)
-#plt.imshow(generated_image[0, :, :, 0], cmap='gray')
-#plt.show()
-discriminator = models.make_discriminator_model()
-#decision = discriminator(generated_image)
-#print (decision)
+    """Load and prepare the [MNIST dataset](http://yann.lecun.com/exdb/mnist/). Convert the samples from integers to floating-point numbers:"""
 
+    #mnist = tf.keras.datasets.mnist
+    cifar10 = tf.keras.datasets.cifar10
 
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
-## Save Checkpoints
-checkpoint_dir = './training_checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                 discriminator_optimizer=discriminator_optimizer,
-                                 generator=generator,
-                                 discriminator=discriminator)
-## Training Loop
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train, x_test = data_util.cast_to_float32 ( x_train ) , data_util.cast_to_float32( x_test )
+    (x_train, y_train), (x_test, y_test) = (x_train[:12000,:], y_train[:12000,:]), (x_test[:100,:], y_test[:100,:])
 
-EPOCHS = cfg.nof_epochs
-noise_dim = 100
-num_examples_to_generate = 16
+    #print(x_train.shape)
+    train_images = (x_train - 127.5) / 127.5  # Normalize the images to [-1, 1]
+
+    # Batch and shuffle the data
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(cfg.BUFFER_SIZE).batch(cfg.BATCH_SIZE)
+
+    #inputs = keras.Input(shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
+    """
+    base_model = applications.resnet50.ResNet50(weights=None, include_top=False,
+                                                input_shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
+    discriminator = tf.keras.applications.ResNet50(
+        include_top=False, weights='imagenet',
+        input_shape=(32,32,3), input_tensor=inputs,pooling=None
+    )"""
 
 
-# You will reuse this seed overtime (so it's easier)
-# to visualize progress in the animated GIF)
-seed = tf.random.normal([num_examples_to_generate, noise_dim])
-
-# Notice the use of `tf.function`
-# This annotation causes the function to be "compiled".
-@tf.function
-def train_step(images):
-    noise = tf.random.normal([cfg.BATCH_SIZE, noise_dim])
-
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-      generated_images = generator(noise, training=True)
-
-      real_output = discriminator(images, training=True)
-      fake_output = discriminator(generated_images, training=True)
-
-      gen_loss = models.generator_loss(fake_output)
-      disc_loss = models.discriminator_loss(real_output, fake_output)
-
-    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
 
-def train(dataset, epochs):
-  for epoch in range(epochs):
-    start = time.time()
 
-    for image_batch in dataset:
-      train_step(image_batch)
+    #noise = tf.random.normal([1, 100])
+    generator = models.make_generator_model(latent_dim=100)
+    # summarize the model
+    generator.summary()
 
-    # Produce images for the GIF as you go
-    display.clear_output(wait=True)
-    generate_and_save_images(generator,
-                             epoch + 1,
-                             seed)
-
-    # Save the model every 15 epochs
-    if (epoch + 1) % 15 == 0:
-      checkpoint.save(file_prefix = checkpoint_prefix)
-
-    print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
-
-  # Generate after the final epoch
-  display.clear_output(wait=True)
-  generate_and_save_images(generator,
-                           epochs,
-                           seed)
-
-def generate_and_save_images(model, epoch, test_input):
-  # Notice `training` is set to False.
-  # This is so all layers run in inference mode (batchnorm).
-  predictions = model(test_input, training=False)
-
-  fig = plt.figure(figsize=(4, 4))
-
-  for i in range(predictions.shape[0]):
-      plt.subplot(4, 4, i+1)
-      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-      plt.axis('off')
-
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-  plt.show()
-
-#Image display
-
-def display_image(epoch_no):
-  return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+    #generated_image = generator(noise, training=False)
+    #print(generated_image[0, :, :, 0].shape)
+    #print(x_train[0,:].shape)
+    #plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+    #plt.show()
+    discriminator = models.make_discriminator_model()
+    #decision = discriminator(generated_image)
+    #print (decision)
 
 
-#call train
-train(train_dataset, cfg.nof_epochs)
-checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-# Display a single image using the epoch number
-display_image(EPOCHS)
-# create Animation
-anim_file = 'dcgan.gif'
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    ## Save Checkpoints
+    checkpoint_dir = './training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                     discriminator_optimizer=discriminator_optimizer,
+                                     generator=generator,
+                                     discriminator=discriminator)
+    ## Training Loop
 
-with imageio.get_writer(anim_file, mode='I') as writer:
-  filenames = glob.glob('image*.png')
-  filenames = sorted(filenames)
-  for filename in filenames:
-    image = imageio.imread(filename)
-    writer.append_data(image)
-  image = imageio.imread(filename)
-  writer.append_data(image)
+    EPOCHS = cfg.nof_epochs
+    noise_dim = 100
+    num_examples_to_generate = 9
+
+
+    # You will reuse this seed overtime (so it's easier)
+    # to visualize progress in the animated GIF)
+    seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+    # Notice the use of `tf.function`
+    # This annotation causes the function to be "compiled".
+    @tf.function
+    def train_step(images):
+        noise = tf.random.normal([cfg.BATCH_SIZE, noise_dim])
+
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+          generated_images = generator(noise, training=True)
+
+          real_output = discriminator(images, training=True)
+          fake_output = discriminator(generated_images, training=True)
+
+          gen_loss = models.generator_loss(fake_output)
+          disc_loss = models.discriminator_loss(real_output, fake_output)
+
+        gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+        generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+        discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+
+    def train(dataset, epochs):
+      for epoch in range(epochs):
+        start = time.time()
+
+        for image_batch in dataset:
+          train_step(image_batch)
+
+        # Produce images for the GIF as you go
+        display.clear_output(wait=True)
+        generate_and_save_images(generator,
+                                 epoch + 1,
+                                 seed)
+
+        # Save the model every 15 epochs
+        if (epoch + 1) % 15 == 0:
+          checkpoint.save(file_prefix = checkpoint_prefix)
+
+        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+      # Generate after the final epoch
+      display.clear_output(wait=True)
+      generate_and_save_images(generator,
+                               epochs,
+                               seed)
+
+    def generate_and_save_images(model, epoch, test_input):
+      # Notice `training` is set to False.
+      # This is so all layers run in inference mode (batchnorm).
+      predictions = model(test_input, training=False)
+
+      fig = plt.figure(figsize=(6, 6))
+
+      for i in range(predictions.shape[0]):
+          plt.subplot(3, 3, i+1)
+          plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+          plt.axis('off')
+
+      plt.savefig('images/image_at_epoch_{:04d}.png'.format(epoch))
+      #plt.show()
+      plt.clf()
+
+    #Image display
+
+    def display_image(epoch_no):
+      return PIL.Image.open('images/image_at_epoch_{:04d}.png'.format(epoch_no))
+
+
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    #call train
+    train(train_dataset, cfg.nof_epochs)
+    checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+    # Display a single image using the epoch number
+    display_image(EPOCHS)
+    # create Animation
+    anim_file = 'dcgan.gif'
+
+    with imageio.get_writer(anim_file, mode='I') as writer:
+      filenames = glob.glob('images/image*.png')
+      filenames = sorted(filenames)
+      for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+      image = imageio.imread(filename)
+      writer.append_data(image)
 
 import tensorflow_docs.vis.embed as embed
 embed.embed_file(anim_file)
