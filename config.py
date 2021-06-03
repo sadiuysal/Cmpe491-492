0d810afd430f91a97b4062339391341a8934a3ef
I540653 <sadi.uysal@sap.com>
2021-06-03 03:19:24 +0300

    new generator added.

--- config.py
+++ config.py
@@ -6,20 +6,10 @@ attacks_itr_count = 7
 attacks_step_size = 1.8
 
 ##
-_BATCH_NORM_DECAY = 0.997
-_BATCH_NORM_EPSILON = 1e-5
 IMG_SIZE = 32
-c_dim = 3 # nof channels
-gf_dim = 64 # Number of filters to use for generator.
-random_seed = 20180526 #
-batch_size = 128 # default 64
+BATCH_SIZE = 256 # default 64
 class_num = 10
 epsilon = 0.0625  # 0-255>>8
-weight_decay = 1e-5
-learning_rate = 0.1
-gamma = 1e-2 #Regularization parameter for gradient regularization
-print_iter = 100 #Number of iterations to print results.
-save_iter = 5 #20000 # Number of iterations to save model.
-nsteps = 20000#0 #Number of steps to run training.
-model_fileG = './savedModels/modelG_cifar10.ckpt' #
-log_dir = './logs_cifar10'#
+
+##
+BUFFER_SIZE = 60000
--- run.py
+++ run.py
@@ -13,14 +13,11 @@
 # limitations under the License.
 import matplotlib.pyplot as plt
 import tensorflow as tf
-#import model as model_class
 from tensorflow import keras
 import objective as loss
 import config as cfg
 import data_util
 import models
-#import tensorflow_addons as tfa
-
 
 """Load and prepare the [MNIST dataset](http://yann.lecun.com/exdb/mnist/). Convert the samples from integers to floating-point numbers:"""
 
@@ -30,300 +27,29 @@ cifar10 = tf.keras.datasets.cifar10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train, x_test = data_util.cast_to_float32 ( x_train ) , data_util.cast_to_float32( x_test )
 (x_train, y_train), (x_test, y_test) = (x_train[:1000,:]/255, y_train[:1000,:]), (x_test[:100,:]/255, y_test[:100,:])
-"""with tf.Session() as sess:
-    print(x_train[0,:].eval())"""
 
-image_batch = tf.train.shuffle_batch(
-                [x_train, y_train],
-                batch_size=cfg.batch_size,
-                num_threads=16,
-                capacity=10000 + 3 * cfg.batch_size,
-                min_after_dequeue=10000,
-                enqueue_many=True
-            )
-
-test_image_batch = tf.train.shuffle_batch(
-    [x_test, y_test],
-    batch_size=cfg.batch_size,
-    num_threads=16,
-    capacity=10000 + 3 * cfg.batch_size,
-    min_after_dequeue=10000,
-    enqueue_many=True
-)
-
-
-#tf.config.run_functions_eagerly(True)
-#tf.compat.v1.disable_eager_execution()
-
-
-
-# data augmentation layers
-#data_augmentation = model_class.data_augmentation
+# Batch and shuffle the data
+train_dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(cfg.BUFFER_SIZE).batch(cfg.BATCH_SIZE)
 
 inputs = keras.Input(shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
 """
 base_model = applications.resnet50.ResNet50(weights=None, include_top=False,
-                                            input_shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))"""
+                                            input_shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
 discriminator = tf.keras.applications.ResNet50(
     include_top=False, weights='imagenet',
     input_shape=(32,32,3), input_tensor=inputs,pooling=None
-)
-layer_to_logits=tf.keras.layers.Dense(10,activation='relu')
-
-generator = models.get_generator(c_dim=cfg.c_dim, f_dim=cfg.gf_dim)
-#print(generator(x_train[:5,:]).shape)
-
-"""Build the `tf.keras.Sequential` model by stacking layers. Choose an optimizer and loss function for training:"""
-#model = model_class.CustomModel(inputs, model_class.model_layers(inputs))
-
-#model = model_class.CustomModel(inputs,discriminator(generator))
-
-
-
-def train(generator,discriminator,data,test_data):
-    tf.set_random_seed(cfg.random_seed)
-
-    batch_size = cfg.batch_size
-    epsilon = cfg.epsilon    # size of perturbation
-    class_num = cfg.class_num    # number of output classes
-    weight_decay = cfg.weight_decay
-    learning_rate = cfg.learning_rate
-    gamma = cfg.gamma    # gradient regularization parameter
-    train_size = x_train.shape[0]    # training set size
-
-    print_iter = cfg.print_iter
-    save_iter = cfg.save_iter
-
-
-    x_clean = data[0]
-    label = data[1]
-
-    # Data augmentation for CIFAR10
-    x_real = tf.map_fn(lambda x: tf.image.resize_image_with_crop_or_pad(x, 32+4, 32+4), x_clean)
-    x_real = tf.map_fn(lambda x: tf.random_crop(x, [32,32,3]), x_real)
-    x_real = tf.map_fn(lambda x: tf.image.random_flip_left_right(x), x_real)
-
-    # Normalize to range [-1,1]
-    x_real = 2.*x_real - 1.
-    x_noise = generator(x_real)
-    x_perturbed = x_real + epsilon * x_noise
-    x_all=tf.concat([x_clean,x_real, x_perturbed], 0)
-
-
-
-    """d_out_real = layer_to_logits(discriminator(x_real))
-    d_out_noise = layer_to_logits(discriminator(x_perturbed))
-    d_loss_real = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-        logits=d_out_real, labels=tf.one_hot(label, class_num)))
-    d_loss_noise = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-        logits=d_out_noise, labels=tf.one_hot(label, class_num)))"""
-    output = discriminator(x_all)
-    """print("x_real: ",x_real.shape)
-    print("x_all: ",x_all.shape)
-    print("x_all_out: ",output.shape)"""
-    # Losses for the discriminator network and generator network
-    # d_loss = d_loss_real + d_loss_noise
-    #g_loss = -d_loss_noise
-    g_loss = -loss.contrastive_Loss(output)
-
-    # Weight decay: assume weights are named 'kernel' or 'weights'
-    g_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
-    #g_vars = tf.trainable_variables(scope='generator')
-
-    g_decay = weight_decay * 0.5 * sum(
-        tf.reduce_sum(tf.square(v)) for v in g_vars if (v.name.find('kernel') > 0 or v.name.find('weights') > 0)
-    )
-
-
-
-
-
-    g_optimizer = tf.train.AdamOptimizer(learning_rate=0.002, beta1=0.5, beta2=0.999)
-
-
-    #g_grads=tape.gradient(g_loss, g_vars)
-    g_grads = tf.gradients(g_loss + g_decay, g_vars, stop_gradients=g_vars)
-    g_train_op = g_optimizer.apply_gradients(zip(g_grads,g_vars))
-    with tf.control_dependencies([g_train_op]):
-        x_noise2 = generator(x_real)
-        x_perturbed2 = x_real + epsilon*x_noise2
-        x_all2 = tf.concat([x_clean, x_real, x_perturbed2], 0)
-        output2 = discriminator(x_all2)
-        g_loss2 = -loss.contrastive_Loss(output2)
-
-        g_grads2 = tf.gradients(g_loss2 + g_decay, g_vars, stop_gradients=g_vars)
-        g_train_op2 = g_optimizer.apply_gradients(zip(g_grads2, g_vars))
-
-        with tf.control_dependencies([g_train_op2]):
-            x_noise3 = generator(x_real)
-            x_perturbed3 = x_real + epsilon*x_noise3
-            x_all3 = tf.concat([x_clean, x_real, x_perturbed3], 0)
-            output3 = discriminator(x_all3)
-            g_loss3 = -loss.contrastive_Loss(output3)
-
-            g_grads3 = tf.gradients(g_loss3 + g_decay, g_vars, stop_gradients=g_vars)
-            g_train_op3 = g_optimizer.apply_gradients(zip(g_grads3, g_vars))
-
-
-            """with tf.control_dependencies([g_train_op3]):
-                x_noise4 = generator(x_real)
-                x_perturbed4 = x_real + epsilon*x_noise4
-                x_all4 = tf.concat([x_clean, x_real, x_perturbed4], 0)
-                output4 = discriminator(x_all4)
-                g_loss4 = loss.contrastive_Loss(output4)
-
-
-                g_grads4 = tf.gradients(g_loss4 + g_decay, g_vars, stop_gradients=g_vars)
-                g_train_op4 = g_optimizer.apply_gradients(zip(g_grads4, g_vars))
-
-
-                with tf.control_dependencies([g_train_op4]):
-                    x_noise5 = generator(x_real)
-                    x_perturbed5 = x_real + epsilon*x_noise5
-                    x_all5 = tf.concat([x_clean, x_real, x_perturbed5], 0)
-                    output5 = discriminator(x_all5)
-                    g_loss5 = loss.contrastive_Loss(output5)
-
-                    g_grads5 = tf.gradients(g_loss5 + g_decay, g_vars, stop_gradients=g_vars)
-                    g_train_op5 = g_optimizer.apply_gradients(zip(g_grads5, g_vars))
-
-
-                    with tf.control_dependencies([g_train_op5]):
-                        x_noise6 = generator(x_real)
-                        x_perturbed6 = x_real + epsilon*x_noise6
-                        x_all6 = tf.concat([x_clean, x_real, x_perturbed6], 0)
-                        output6 = discriminator(x_all6)
-                        g_loss6 = loss.contrastive_Loss(output6)"""
-            g_grads3 = tf.gradients(g_loss3 + g_decay, g_vars, stop_gradients=g_vars)
-            g_train_op3 = g_optimizer.apply_gradients(zip(g_grads3, g_vars))
-            #g_grads6 = tf.gradients(g_loss6 + g_decay, g_vars, stop_gradients=g_vars)
-
-
-    # D step: use finite approx for gradient regularization
-
-    # evaluate Hessian vector approx (Eq. 9 in paper)
-    # g_virtual_optimizer computes \phi_k + hv
-    g_step_size = learning_rate/10
-    g_virtual_optimizer = tf.train.GradientDescentOptimizer(learning_rate=g_step_size)
-
-    #with tf.control_dependencies([d_grads_noise6[0], d_grads_real[0]]):
-    g_virtual_step = g_virtual_optimizer.apply_gradients(zip(g_grads3, g_vars))
-
-    # make sure g is updated before re-computing
-    with tf.control_dependencies([g_virtual_step]):
-        x_noiseB = generator(x_real)
-
-    x_perturbedB = tf.stop_gradient(x_real + epsilon*x_noiseB)
-    x_allB = tf.concat([x_clean, x_real, x_perturbedB], 0)
-    outputB = discriminator(x_allB)
-    g_lossB = -loss.contrastive_Loss(outputB)
-    #print("Generator loss after iteration: " ,g_lossB)
-
-
-
-
-    # apply D updates
-
-    # restore parameters for G
-    #minus_g_grads = [-g for g in g_grads6]
-
-    #d_train_and_restore_op = g_virtual_optimizer.apply_gradients(zip(minus_g_grads, g_vars)
-
-    # Gradient norm to evaluate convergence
-    g_reg = 0.5 * sum(
-        tf.reduce_sum(tf.square(g)) for g in g_grads3
-    )
-    # Build test
-    acc_g, acc_update_g, acc_init_g = build_test_G(discriminator, generator, test_data)
-    #acc_g, acc_init_g = build_test_G(discriminator, generator, test_data)
-    # Global step
-    global_step = tf.Variable(0, trainable=False)
-    global_step_op = global_step.assign_add(1)
-
-    saverG = tf.train.Saver(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator'))
-
-    # Supervisor
-    sv = tf.train.Supervisor(
-        logdir=cfg.log_dir, global_step=global_step,
-        summary_op=None,
-    )
-
-    model_filenameG = cfg.model_fileG
-
-    with sv.managed_session() as sess:
-
-        num_steps_per_epoch = int(train_size // (batch_size)) + 1
-
-        for batch_idx in range(cfg.nsteps):
-            if sv.should_stop():
-                break
-
-            g_loss_out,g_loss_outB = sess.run(
-                [g_loss,g_lossB])
-
-            print("Batch: "+str(batch_idx)+" Generator loss : " + str(g_loss_out))
-            print("Batch: " + str(batch_idx) + " Generator loss after iteration : " + str(g_loss_outB))
-            if batch_idx % print_iter == 0:
-                #test_acc = run_test(acc, acc_update, acc_init, sess, config)
-                #test_acc_fgs = run_test(acc_fgs, acc_update_fgs, acc_init_fgs, sess, config)
-                #test_acc_pgd = run_test(acc_pgd, acc_update_pgd, acc_init_pgd, sess, config)
-                test_acc_g = run_test(acc_g, acc_update_g, acc_init_g, sess)
-
-                print('i=%d, Loss_g: %4.4f, acc_g: %.4f' % (batch_idx, g_loss_out , test_acc_g),
-                      flush=True)
-
-            if batch_idx % save_iter == 0:
-                #saverD.save(sess, model_filenameD)
-                saverG.save(sess, model_filenameG)
-
-            sess.run(global_step_op)
-
-        #saverD.save(sess, model_filenameD)
-        saverG.save(sess, model_filenameG)
-
-
-# construct graph for testing on examples perturbed by G
-def build_test_G(discriminator, generator, test_data):
-    x = test_data[0]
-    x = 2.0*x - 1.0
-    y = test_data[1]
-    epsilon = cfg.epsilon
-
-    x_noise = tf.stop_gradient(generator(x))
-    x_perturbed = x + epsilon*x_noise
-
-    d_out = layer_to_logits(discriminator(x_perturbed))
-    #d_out = tf.squeeze(d_out, [2])
-    predictions = tf.argmax(d_out, 1)
-    y = tf.one_hot(y, 10)
-
-    tf_metric, tf_metric_update = tf.metrics.accuracy(y, predictions, name='g_metric')
-    running_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope="g_metric")
-    running_vars_initializer = tf.variables_initializer(var_list=running_vars)
-
-
-    return (tf_metric, tf_metric_update, running_vars_initializer)
-
-# run actual test to collect performance statistics
-def run_test(tf_metric, tf_metric_update, running_vars_initializer, sess):
-    #print("run_test1")
-    sess.run(running_vars_initializer)
-
-    batch_size = cfg.batch_size
-    test_size = x_test.shape[0]
-    num_steps = int(test_size//batch_size)+1
-    for i in range(num_steps):
-        sess.run(tf_metric_update)
-    accuracy = sess.run(tf_metric)
-    return accuracy
-
-
-
-#x_train, y_train = x_train[-(2*cfg.batch_size):], y_train[-(2*cfg.batch_size):]
-#train(training_data=x_train)
-
-
-
-train(generator,discriminator,image_batch,test_image_batch)
+)"""
+
+
+noise = tf.random.normal([1, 100])
+generator = models.make_generator_model(latent_dim=100)
+# summarize the model
+generator.summary()
+
+#generated_image = generator(noise, training=False)
+#print(generated_image[0, :, :, 0].shape)
+#print(x_train[0,:].shape)
+#plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+#plt.show()
 
 
