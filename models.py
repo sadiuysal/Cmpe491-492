ccf4ff1def6ec1e571657d01fd14fd9a8869faeb
I540653 <sadi.uysal@sap.com>
2021-06-19 04:35:30 +0300

    feedbacks applied, classfier-training added

--- models.py
+++ models.py
@@ -1,15 +1,21 @@
 import tensorflow as tf
+from tensorflow.python.keras.layers import GlobalAveragePooling2D, Dropout
+from tensorflow.python.keras.optimizer_v2.adam import Adam
+
 import objective as losses
 from tensorflow.keras.layers.experimental import preprocessing
 import data_util
 from tensorflow.keras import layers, Model, Input
+import config as cfg
+import os
+from tensorflow.python.keras.losses import SparseCategoricalCrossentropy
 
 
 # This method returns a helper function to compute cross entropy loss
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=False)
 
-tf.config.threading.set_inter_op_parallelism_threads(3)
-tf.config.threading.set_intra_op_parallelism_threads(3)
+tf.config.threading.set_inter_op_parallelism_threads(6)
+tf.config.threading.set_intra_op_parallelism_threads(6)
 
 
 
@@ -24,18 +30,19 @@ def make_generator_model(latent_dim):
     model.add(layers.Reshape((8, 8, 256)))
 
     # upsample to 16x16
-    model.add(layers.Conv2DTranspose(128, (6, 6), strides=(1, 1), padding='same',use_bias=False))
+    #TODO change kernel size to 4
+    model.add(layers.Conv2DTranspose(128, (4, 4), strides=(1, 1), padding='same',use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     # upsample to 32x32
-    model.add(layers.Conv2DTranspose(64, (6, 6), strides=(2, 2), padding='same',use_bias=False))
+    model.add(layers.Conv2DTranspose(64, (4, 4), strides=(2, 2), padding='same',use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     #print("Before Out layer dimension : ",model.output_shape )
     # output layer
-    model.add(layers.Conv2DTranspose(3, (6, 6),strides=(2, 2), activation='tanh', padding='same',use_bias=False))
+    model.add(layers.Conv2DTranspose(3, (4, 4),strides=(2, 2), activation='tanh', padding='same',use_bias=False))
     #print("After Out layer dimension : ", model.output_shape)
 
 
@@ -48,45 +55,35 @@ def make_generator_model(latent_dim):
 
 def make_discriminator_model():
     in_shape = (32, 32, 3)
-    #model = Sequential()
-    """
-    # normal
-    model.add(Conv2D(64, (6, 6),strides=(2, 2), padding='same', input_shape=in_shape))
-    model.add(LeakyReLU())
-    model.add(Dropout(0.3))
-    # downsample
-    model.add(Conv2D(128, (6, 6), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU())
-    model.add(Dropout(0.3))
-    print("1-Out layer dimension : ",model.output_shape )
-    # classifier
-    model.add(Flatten())
-    print("2-Out layer dimension : ",model.output_shape )
-    model.add(Dense(1))
-    print("3-Out layer dimension : ", model.output_shape)
-    #return model"""
-
-    """"# compile model ##LOOK HERE
-    opt = Adam(lr=0.0002, beta_1=0.5)
-    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])"""
     inputs = Input(shape=(32, 32, 3))
-    base_model = tf.keras.applications.ResNet50(
+    backbone = tf.keras.applications.ResNet50(
         include_top=False, weights='imagenet',input_shape=in_shape,input_tensor=inputs,
         pooling="avg"
     )
+    backbone_ENetB0 = tf.keras.applications.efficientnet.EfficientNetB0(
+        include_top=False, weights='imagenet', input_tensor=inputs,
+        input_shape=in_shape
+    )
+    # Freeze the pretrained weights
+    backbone_ENetB0.trainable = False
+    print("Backbone ENetB0 out shape: ",backbone_ENetB0.output_shape)
+
+
+    x = GlobalAveragePooling2D()(backbone_ENetB0.output)
+    x = layers.BatchNormalization()(x)
+    x = layers.Dropout(0.2, name="top_dropout")(x)
+    outputs = layers.Dense(cfg.class_num,activation="softmax")(x)
+    classifier_ENetB0 = Model(inputs=backbone_ENetB0.input, outputs=outputs)
+    print("Classifier ENetB0 out shape: ",classifier_ENetB0.output_shape)
+
+    return backbone_ENetB0,classifier_ENetB0
+
     #Flatten output layer of Resnet
     #flattened = Flatten()(base_model.output)
-    dense_layer =layers.Dense(1,activation="sigmoid")(base_model.output)
-    model = Model(inputs=base_model.input, outputs=dense_layer)
+    #dense_layer =layers.Dense(cfg.class_num,activation="softmax")(base_model.output)
+    #model = Model(inputs=base_model.input, outputs=dense_layer)
 
-    #print("*1-Out layer dimension : ", model.output_shape)
-    #x = base_model.output
-    #x = Dropout(0.3)(x)
-    #x = Flatten()(x)
-    #predictions = Dense(1, activation='softmax')(x)
-    #model_pretrained = Model(inputs=base_model.input, outputs=predictions)
-
-    return model
+    #return model
 
 
 def discriminator_loss(real_output, fake_output):
@@ -95,9 +92,40 @@ def discriminator_loss(real_output, fake_output):
     total_loss = real_loss + fake_loss
     return total_loss
 
-def generator_loss(org_images,aug_images,adv_images):
-    return -losses.contrastive_Loss(tf.concat([org_images,aug_images,adv_images], 0))
+def generator_loss(backbone_model,org_images,aug_images,adv_images):
+    #TODO use Resnet backbone to get features
+    out = backbone_model(tf.concat([org_images,aug_images,adv_images], 0))
+    return -losses.contrastive_Loss(out)
     #return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+def fineTuneonCifar10(classifier,x_train,y_train,isLoad):
+    # Save finetuned model with callback
+    checkpoint_path = "finetuned/cp.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    # Create a callback that saves the model's weights
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                     save_weights_only=True,
+                                                     verbose=1)
+    if isLoad:
+        # Loads the weights
+        classifier.load_weights(checkpoint_path)
+        classifier.trainable = False
+    else:
+        # Loads the weights
+        #classifier.load_weights(checkpoint_path)
+        # Finetuning on cifar10
+        loss_fn = SparseCategoricalCrossentropy(from_logits=False)
+        #classifier.summary()
+        classifier.trainable = True
+        for l in classifier.layers[:-3]:
+            l.trainable = False
+        classifier.compile(optimizer=Adam(learning_rate=1e-4, decay=1e-4 / 150), loss=loss_fn)
+        classifier.fit(x_train, y_train, callbacks=[cp_callback],epochs=150)
+
+    return classifier
+
+
 
 #data augmentation layers
 data_augmentation = tf.keras.Sequential([
--- run.py
+++ run.py
@@ -28,11 +28,18 @@ from IPython import display
 
 
 
-
-
 logical_devices = tf.config.list_logical_devices('CPU')
 print("Num CPUs:", len(logical_devices))
 
+
+def test_accuracy_2(images, labels, model):
+    predictions = model(images)
+
+    prediction_labels = tf.math.argmax(predictions, 1)
+    print("Accuracy score: ")
+    m = tf.keras.metrics.Accuracy()
+    m.update_state(labels, prediction_labels)
+    print(m.result())
 
 with tf.device(logical_devices[0].name):
 
@@ -43,53 +50,39 @@ with tf.device(logical_devices[0].name):
 
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     x_train, x_test = data_util.cast_to_float32 ( x_train ) , data_util.cast_to_float32( x_test )
-    (x_train, y_train), (x_test, y_test) = (x_train[:10000,:], y_train[:10000,:]), (x_test[:100,:], y_test[:100,:])
+    #(x_train, y_train), (x_test, y_test) = (x_train[:300,:], y_train[:300,:]), (x_test[:100,:], y_test[:100,:])
 
     #print(x_train.shape)
     train_images = (x_train - 127.5) / 127.5  # Normalize the images to [-1, 1]
     #TODO change later on
-    test_images = (x_train - 127.5) / 127.5  # Normalize the images to [-1, 1]
+    test_images = x_test
+    test_labels = y_test
 
     # Batch and shuffle the data
     train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(cfg.BUFFER_SIZE).batch(cfg.BATCH_SIZE)
 
-    #inputs = keras.Input(shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
-    """
-    base_model = applications.resnet50.ResNet50(weights=None, include_top=False,
-                                                input_shape=(cfg.IMG_SIZE, cfg.IMG_SIZE, 3))
-    discriminator = tf.keras.applications.ResNet50(
-        include_top=False, weights='imagenet',
-        input_shape=(32,32,3), input_tensor=inputs,pooling=None
-    )"""
-
     noise = tf.random.normal([1, 100])
     generator = models.make_generator_model(latent_dim=100)
-    # summarize the model
-    #generator.summary()
 
-    #generated_image = generator(noise, training=False)
-    #print(generated_image.shape)
-    #print(x_train[0,:].shape)
-    #plt.imshow((generated_image[0, :, :, :] * 127.5 + 127.5 )/255)
-    #plt.show()
-    discriminator = models.make_discriminator_model()
-    #decision = discriminator(generated_image)
-    #print (decision)
+    backbone,classifier = models.make_discriminator_model()
+    test_accuracy_2(test_images,test_labels,classifier)
+    classifier = models.fineTuneonCifar10(classifier,x_train,y_train,isLoad=False)
+    test_accuracy_2(test_images, test_labels, classifier)
+
+
 
 
     generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    #discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
     ## Save Checkpoints
     checkpoint_dir = './training_checkpoints'
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
     checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                     discriminator_optimizer=discriminator_optimizer,
-                                     generator=generator,
-                                     discriminator=discriminator)
+                                     generator=generator)
 
     #loss tracker
     g_loss_tracker = metrics.Mean(name="loss")
-    d_loss_tracker = metrics.Mean(name="loss")
+    #d_loss_tracker = metrics.Mean(name="loss")
 
     ## Training Loop
 
@@ -102,7 +95,7 @@ with tf.device(logical_devices[0].name):
     # to visualize progress in the animated GIF)
     seed = tf.random.normal([num_examples_to_generate, noise_dim])
     x_test_sample=test_images[:num_examples_to_generate,:]
-    y_test_sample=y_test[:num_examples_to_generate,:]
+    y_test_sample=test_labels[:num_examples_to_generate,:]
 
     # Notice the use of `tf.function`
     # This annotation causes the function to be "compiled".
@@ -115,11 +108,11 @@ with tf.device(logical_devices[0].name):
             adv_images = tf.clip_by_value(images+cfg.epsilon*image_noises,-1,1)
             aug_images = tf.clip_by_value(data_augmentation(images), -1, 1)
 
-            real_output = discriminator(images, training=False)
-            fake_output = discriminator(adv_images, training=False)
+            #real_output = discriminator(images, training=False)
+            #fake_output = discriminator(adv_images, training=False)
 
-            gen_loss = models.generator_loss(images,aug_images,adv_images)
-            disc_loss = models.discriminator_loss(real_output, fake_output)
+            gen_loss = models.generator_loss(backbone,images,aug_images,adv_images)
+            #disc_loss = models.discriminator_loss(real_output, fake_output)
 
 
         #print("Generator loss: ", gen_loss)
@@ -132,13 +125,13 @@ with tf.device(logical_devices[0].name):
         #discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
         g_loss_tracker.update_state(gen_loss)
-        d_loss_tracker.update_state(disc_loss)
+        #d_loss_tracker.update_state(disc_loss)
         # Return a dict mapping metric names to current value
-        return {"g_loss": [g_loss_tracker.result() for m in [g_loss_tracker]],
-                "d_loss": [d_loss_tracker.result() for m in [d_loss_tracker]]}
+        return {"g_loss": [g_loss_tracker.result() for m in [g_loss_tracker]] }
 
 
     def train(dataset, epochs):
+      epoch_avg_time = 0
       for epoch in range(epochs):
         start = time.time()
 
@@ -152,14 +145,22 @@ with tf.device(logical_devices[0].name):
                                  epoch + 1,
                                  seed)
 
+
+        epoch_avg_time += time.time() - start
+
         # Save the model every 5 epochs
         if (epoch + 1) % 5 == 0:
           checkpoint.save(file_prefix = checkpoint_prefix)
-          print("Generator loss: " + str(g_loss_tracker.result()) + "  Discriminator loss: " + str(
-              d_loss_tracker.result()))
-          print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
+          print("Epoch "+str(epoch+1)+" Generator loss: " + str(g_loss_tracker.result()))
+          epoch_avg_time /= 5
+          print('Avg time for 5 epoch is {} sec'.format(epoch_avg_time))
+          epoch_avg_time = 0
+          test_accuracy(test_images,test_labels,classifier)
 
-        #print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+        if epoch == 0:
+            print('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+            test_accuracy(test_images, test_labels, classifier)
 
       # Generate after the final epoch
       display.clear_output(wait=True)
@@ -181,16 +182,36 @@ with tf.device(logical_devices[0].name):
         #plt.show()
 
 
+    def test_accuracy(images,labels,model):
+        predictions = model(images)
+
+        prediction_labels = tf.math.argmax(predictions, 1)
+        #print(predictions[:10,:])
+        #print(prediction_labels[:10])
+        #print("True labels: ", labels[:10])
+        print("Accuracy score on given test data: ")
+        m = tf.keras.metrics.Accuracy()
+        m.update_state(labels,prediction_labels)
+        print(m.result())
+        #accuracy_score(test_labels, prediction_labels)
+
+
+
     #Image display
 
     def display_image(epoch_no):
       return PIL.Image.open('images/image_at_epoch_{:04d}.png'.format(epoch_no))
 
 
+
+
     if not os.path.exists('images'):
         os.makedirs('images')
+
     #call train
     #checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+"""
     train(train_dataset, EPOCHS)
     checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
     # Display a single image using the epoch number
@@ -208,7 +229,8 @@ with tf.device(logical_devices[0].name):
       writer.append_data(image)
 
 import tensorflow_docs.vis.embed as embed
-embed.embed_file(anim_file)
+embed.embed_file(anim_file)"""
+
 
 
 
